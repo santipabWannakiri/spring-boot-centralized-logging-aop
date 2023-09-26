908830e6a8be37edcc48e473f20dc9f932d61b24
Nokia3310 <santipab1991@gmail.com>
2023-09-26 22:35:34 +0700

    Project Initiation

---
+++ src/main/java/com/centralized/logging/aop/SpringBootCentralizedLoggingAopApplication.java
@@ -0,0 +1,13 @@
+package com.centralized.logging.aop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCentralizedLoggingAopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootCentralizedLoggingAopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/centralized/logging/aop/SpringBootCentralizedLoggingAopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.centralized.logging.aop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootCentralizedLoggingAopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
