6587674af63ea7440ea538a8be9b41dd5a1204cf
Nokia3310 <santipab1991@mail.com>
2023-09-28 23:43:20 +0700

    Update LoggingAspect to capture log service level

--- src/main/java/com/centralized/logging/aop/configuration/LoggingAspect.java
+++ src/main/java/com/centralized/logging/aop/configuration/LoggingAspect.java
@@ -1,5 +1,6 @@
 package com.centralized.logging.aop.configuration;
 
+import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
@@ -8,19 +9,22 @@ import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
+@Slf4j
 public class LoggingAspect {
 
-    @Before("execution(* com.centralized.logging.aop.controller.*.*(..))")
+    @Before("execution(* com.centralized.logging.aop.controller.*.*(..)) || execution(* com.centralized.logging.aop.service.*.*(..))")
     public void logMethodEntry(JoinPoint joinPoint){
         String className = joinPoint.getTarget().getClass().getName();
         String methodName = joinPoint.getSignature().getName();
         System.out.println("Entering " + className + "." + methodName);
+        log.info("Entering {}.{}", className, methodName);
     }
 
-    @After("execution(* com.centralized.logging.aop.controller.*.*(..))")
+    @After("execution(* com.centralized.logging.aop.controller.*.*(..)) || execution(* com.centralized.logging.aop.service.*.*(..))")
     public void logMethodExit(JoinPoint joinPoint){
         String className = joinPoint.getTarget().getClass().getName();
         String methodName= joinPoint.getSignature().getName();
         System.out.println("Exiting " + className + "." + methodName);
+        log.info("Exiting {}.{}", className, methodName);
     }
 }
